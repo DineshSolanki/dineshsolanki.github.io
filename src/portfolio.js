c5dbe7e2747d720fdb89c4c89334991703baeb64
DineshSolanki <15937452+DineshSolanki@users.noreply.github.com>
2025-08-23 16:57:02 +0530

    fix(portfolio): update URLs and refine skills and certifications sections

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,7 +15,7 @@ const seo = {
   og: {
     title: "Dinesh Solanki Portfolio",
     type: "website",
-    url: "https://aprogrammer.wordpress.com/",
+    url: "https://aprogrammers.wordpress.com/",
   },
 };
 
@@ -45,36 +45,24 @@ const socialMediaLinks = [
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
-  /*  {
-    name: "YouTube",
-    link: "https://youtube.com/c/-",
-    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
-    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
-  },*/
   {
     name: "Gmail",
     link: "mailto:solankid297@gmail.com",
-    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+    fontAwesomeIcon: "fa-google",
+    backgroundColor: "#D14836",
   },
-  /*{
-    name: "X-Twitter",
-    link: "https://twitter.com/-",
-    fontAwesomeIcon: "fa-x-twitter", // Reference https://fontawesome.com/icons/x-twitter?f=brands&s=solid
-    backgroundColor: "#000000", // Reference https://simpleicons.org/?q=x
-  },*/
-  /*{
-    name: "Facebook",
-    link: "https://www.facebook.com/-/",
-    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
-  },*/
-  /*{
-    name: "Instagram",
-    link: "https://www.instagram.com/-/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
-  },*/
+  {
+    name: "Medium",
+    link: "https://dineshsolanki.medium.com/",
+    fontAwesomeIcon: "fa-medium",
+    backgroundColor: "#12100E",
+  },
+  {
+    name: "Blog",
+    link: "https://aprogrammers.wordpress.com/",
+    fontAwesomeIcon: "fa-wordpress",
+    backgroundColor: "#21759B",
+  },
 ];
 
 const skills = {
@@ -234,45 +222,17 @@ const skills = {
         },
       ],
     },
-    /*    {
-      title: "UI/UX Design",
-      fileName: "DesignImg",
+    {
+      title: "Other Skills",
+      fileName: "OtherSkillsImg",
       skills: [
-        "⚡ Designing highly attractive user interface for mobile and web applications",
-        "⚡ Customizing logo designs and building logos from scratch",
-        "⚡ Creating the flow of application functionalities to optimize user experience",
+        "⚡ CI",
+        "⚡ Code Review",
+        "⚡ Application Development",
+        "⚡ Languages: English (Full Professional), Spanish (Elementary), Hindi (Native or Bilingual)",
       ],
-      softwareSkills: [
-        {
-          skillName: "Adobe XD",
-          fontAwesomeClassname: "simple-icons:adobexd",
-          style: {
-            color: "#FF2BC2",
-          },
-        },
-        {
-          skillName: "Figma",
-          fontAwesomeClassname: "simple-icons:figma",
-          style: {
-            color: "#F24E1E",
-          },
-        },
-        {
-          skillName: "Adobe Illustrator",
-          fontAwesomeClassname: "simple-icons:adobeillustrator",
-          style: {
-            color: "#FF7C00",
-          },
-        },
-        {
-          skillName: "Inkscape",
-          fontAwesomeClassname: "simple-icons:inkscape",
-          style: {
-            color: "#000000",
-          },
-        },
-      ],
-    },*/
+      softwareSkills: [],
+    },
   ],
 };
 
@@ -362,114 +322,46 @@ const degrees = {
 
 const certifications = {
   certifications: [
-    /*{
-      title: "Machine Learning",
-      subtitle: "- Andrew Ng",
-      logo_path: "stanford_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "Stanford University",
-      color_code: "#8C151599",
-    },
     {
-      title: "Deep Learning",
-      subtitle: "- Andrew Ng",
-      logo_path: "deeplearning_ai_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
-      alt_name: "deeplearning.ai",
-      color_code: "#00000099",
-    },
-    {
-      title: "ML on GCP",
-      subtitle: "- GCP Training",
-      logo_path: "google_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
-      alt_name: "Google",
-      color_code: "#0C9D5899",
-    },
-    {
-      title: "Data Science",
-      subtitle: "- Alex Aklson",
-      logo_path: "ibm_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
-      alt_name: "IBM",
-      color_code: "#1F70C199",
-    },
-    {
-      title: "Big Data",
-      subtitle: "- Kim Akers",
+      title: "Foundational C#",
+      subtitle: "Microsoft",
       logo_path: "microsoft_logo.png",
-      certificate_link:
-        "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+      certificate_link: "",
       alt_name: "Microsoft",
-      color_code: "#D83B0199",
+      color_code: "#D83B01",
     },
     {
-      title: "Advanced Data Science",
-      subtitle: "- Romeo Kienzler",
-      logo_path: "ibm_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
-      alt_name: "IBM",
-      color_code: "#1F70C199",
+      title: "EF SET English Certificate - 77/100",
+      subtitle: "C2 Proficient",
+      logo_path: "efset_logo.png",
+      certificate_link: "",
+      alt_name: "EF SET",
+      color_code: "#0C9D58",
     },
     {
-      title: "Advanced ML on GCP",
-      subtitle: "- GCP Training",
-      logo_path: "google_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
-      alt_name: "Google",
-      color_code: "#0C9D5899",
+      title: "Exela Extra Mile Award",
+      subtitle: "Honors & Awards",
+      logo_path: "exela-logo.png",
+      certificate_link: "",
+      alt_name: "Exela Technologies",
+      color_code: "#000000",
     },
     {
-      title: "DL on Tensorflow",
-      subtitle: "- Laurence Moroney",
-      logo_path: "deeplearning_ai_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
-      alt_name: "deeplearning.ai",
-      color_code: "#00000099",
+      title: "Spot Award 2025",
+      subtitle: "Honors & Awards",
+      logo_path: "award.png",
+      certificate_link: "",
+      alt_name: "Spot Award",
+      color_code: "#FFD700",
     },
     {
-      title: "Fullstack Development",
-      subtitle: "- Jogesh Muppala",
-      logo_path: "coursera_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
-      alt_name: "Coursera",
-      color_code: "#2A73CC",
+      title: "Ness Quarterly Award - Innovation",
+      subtitle: "Honors & Awards",
+      logo_path: "ness-logo.png",
+      certificate_link: "",
+      alt_name: "Ness Digital Engineering",
+      color_code: "#000000",
     },
-    {
-      title: "Kuberenetes on GCP",
-      subtitle: "- Qwiklabs",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
-      alt_name: "GCP",
-      color_code: "#4285F499",
-    },
-    {
-      title: "Cryptography",
-      subtitle: "- Saurabh Mukhopadhyay",
-      logo_path: "nptel_logo.png",
-      certificate_link:
-        "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
-      alt_name: "NPTEL",
-      color_code: "#FFBB0099",
-    },
-    {
-      title: "Cloud Architecture",
-      subtitle: "- Qwiklabs",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
-      alt_name: "GCP",
-      color_code: "#4285F499",
-    },*/
   ],
 };
 
@@ -490,7 +382,7 @@ const experience = {
       work: true,
       experiences: [
         {
-          title: "Senior Software Engineer",
+          title: "Senior Engineer",
           company: "Ness Digital Engineering",
           company_url: "https://www.ness.com/",
           logo_path: "ness-logo.png",
@@ -715,8 +607,8 @@ const contactPageData = {
   blogSection: {
     title: "Blogs",
     subtitle:
-      "I like to document some of my experiences in professional career journey as well as some technical knowledge sharing.",
-    link: "https://aprogrammers.wordpress.com",
+      "I like to document some of my experiences in professional career journey as well as technical knowledge sharing.",
+    link: "https://aprogrammers.wordpress.com/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
@@ -725,10 +617,7 @@ const contactPageData = {
     locality: "Rajasthan",
     country: "INDIA",
     region: "Rajasthan",
-    // postalCode: "95129",
-    // streetAddress: "Saratoga Avenue",
     avatar_image_path: "address_image.svg",
-    // location_map_link: "https://maps.app.goo.gl/NvYZqa34Wye4tpS17",
   },
   phoneSection: {
     title: "",
